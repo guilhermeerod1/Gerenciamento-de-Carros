399fdbe44e1a2bb7de0abd85f46df61abc67fa3e
guilhermeerod1 <guilherme.e.rod@hotmail.com>
2015-11-25 17:21:14 -0200

    Adição dos arquivos do projeto
    Arquivos: Carro.cs (Classe de Modelo), CarroBO.cs (Classe de Negócio),
    CarroDAO (Classe de acesso ao BD), Carro.aspx (Web Form para cadastro e
    edição), Visualizar.aspx (Web Form para visualização e remoção) e
    style.css (Estilos css).

---
+++ Gerenciamento-de-Carros/Classes/CarroBO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gerenciamento_de_Carros.Classes
+{
+    public class CarroBO
+    {
+    }
+}
---
+++ Gerenciamento-de-Carros/Models/Carro.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gerenciamento_de_Carros.Models
+{
+    public class Carro
+    {
+    }
+}
