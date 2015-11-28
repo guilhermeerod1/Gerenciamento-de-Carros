574a5c107eac3a70b9ff3ea51d8928ba6aab7c35
fasm19 <felipe_aumendes16@hotmail.comgit config --global user.name fasm19git commit -m 'Arquivos documentos e cursos adicionados'git config --global user.email felipe_aumendes16@hotmail.com>
2015-11-27 22:28:00 -0200

    Acrescimo do DataBound no Visualizar

--- Gerenciamento-de-Carros/Forms/Visualizar.aspx.cs
+++ Gerenciamento-de-Carros/Forms/Visualizar.aspx.cs
@@ -15,11 +15,10 @@ namespace Gerenciamento_de_Carros.Forms
         {
             if (!Page.IsPostBack)
             {
-                
+                CarroBO carroBO = new CarroBO();
+                List<Carro> listaDeCarros = carroBO.RetornarCarro();
             }
 
-            CarroBO carroBO = new CarroBO();
-            List<Carro> listaDeCarros = carroBO.RetornarCarro();
 
             if (listaDeCarros != null)
             {
@@ -29,6 +28,37 @@ namespace Gerenciamento_de_Carros.Forms
             else
             {
                 lblMensagem.Text = "Nenhum Registro";
+            }
+        }
+
+        protected void Repeater1_ItemDataBound(object sender, System.Web.UI.WebControls.RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == System.Web.UI.WebControls.ListItemType.Item || e.Item.ItemType == System.Web.UI.WebControls.ListItemType.AlternatingItem)
+            {
+                Carro carro = (Carro)e.Item.DataItem;
+
+                Label lblId = (Label)e.Item.FindControl("lblId");
+                Label lblModelo = (Label)e.Item.FindControl("lblModelo");
+                Label lblPreco = (Label)e.Item.FindControl("lblPreco");
+                Label lblAnoModelo = (Label)e.Item.FindControl("lblAnoModelo");
+                Label lblKm = (Label)e.Item.FindControl("lblDataKm");
+                Label lblCor = (Label)e.Item.FindControl("lblDataCor");
+                Label lblCombustivel = (Label)e.Item.FindControl("lblCombustivel");
+
+                HyperLink lnkEditar = (HyperLink)e.Item.FindControl("lnkEditar");
+                HyperLink lnkExcluir = (HyperLink)e.Item.FindControl("lnkExcluir");
+
+                lnkEditar.NavigateUrl = "~/Carros.aspx?ID=" + carro.Id;
+                lnkExcluir.NavigateUrl = "~/Carros.aspx?remover=" + carro.Id;
+
+                lblId.Text = carro.Id.ToString();
+                lblModelo.Text = carro.Modelo.ToString();
+                lblPreco.Text = carro.Preco.ToString();
+                lblAnoModelo.Text = carro.AnoModelo.ToString();
+                lblKm.Text = carro.Km.ToString();
+                lblCor.Text = carro.Cor.ToString();
+                lblCombustivel.Text = carro.Combustivel.ToString();
+
             }
         }
     }
